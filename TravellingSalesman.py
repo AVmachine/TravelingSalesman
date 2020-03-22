be6793133f32aa9e8afe7ce9d0908d5d6f4bdb22
Alex A <alex.av936@gmail.com>
2020-03-22 08:42:28 -0500

    Used mlrose and solved it...
    Too easy, I'll go back and attempt in java.

--- TravellingSalesman.py
+++ TravellingSalesman.py
@@ -1,14 +1,16 @@
 import mlrose
 import numpy as np
 
-coords_list = [(1, 1), (4, 2), (5, 2), (6, 4), (4, 4), (3, 6), (1, 5), (2, 3)]
-
-problem_fit = mlrose.TSPOpt(length = 8, coords = coords_list,
+coords_list = [(2, 2), (4, 4), (6, 5), (7, 9)]
+alpha_list = {1: 'A', 2: 'B', 3: 'C', 4: 'D'}
+problem_fit = mlrose.TSPOpt(length = 4, coords = coords_list,
                             maximize=False)
 
 
-best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state = 2)
+best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state = 0)
 
 print('The best state found is: ', best_state)
 
 print('The fitness at the best state is: ', best_fitness)
+
+
